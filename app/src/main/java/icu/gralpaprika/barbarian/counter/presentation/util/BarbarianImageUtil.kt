0eb2a77bc2c8a773ca161d80d7260198835c43af
CarlosFR4 <cfr4123@gmail.com>
2025-08-22 17:41:26 -0600

    Use the real images for the barbarian.

--- app/src/main/java/icu/gralpaprika/barbarian/counter/presentation/util/BarbarianImageUtil.kt
+++ app/src/main/java/icu/gralpaprika/barbarian/counter/presentation/util/BarbarianImageUtil.kt
@@ -6,18 +6,17 @@ object BarbarianImageUtil {
     
     fun getImageForBarbarianLevel(level: Int): Int {
         return when (level) {
-            0 -> R.drawable.gentleman_level_0
-            1 -> R.drawable.gentleman_level_1
-            2 -> R.drawable.gentleman_level_2
-            3 -> R.drawable.gentleman_level_3
-            4 -> R.drawable.gentleman_level_4
-            5 -> R.drawable.gentleman_level_5
-            6 -> R.drawable.gentleman_level_6
-            7 -> R.drawable.gentleman_level_7
-            8 -> R.drawable.gentleman_level_8
-            9 -> R.drawable.gentleman_level_9
-            10 -> R.drawable.gentleman_level_10
-            else -> R.drawable.gentleman_level_0
+            1 -> R.drawable.barbarian_level_1
+            2 -> R.drawable.barbarian_level_2
+            3 -> R.drawable.barbarian_level_3
+            4 -> R.drawable.barbarian_level_4
+            5 -> R.drawable.barbarian_level_5
+            6 -> R.drawable.barbarian_level_6
+            7 -> R.drawable.barbarian_level_7
+            8 -> R.drawable.barbarian_level_8
+            9 -> R.drawable.barbarian_level_9
+            10 -> R.drawable.barbarian_level_10
+            else -> R.drawable.barbarian_level_0
         }
     }
 }
