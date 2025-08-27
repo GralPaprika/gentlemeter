3ec96451f4d952628d64fd7ffca04c8189ccbf18
CarlosFR4 <cfr4123@gmail.com>
2025-08-27 11:07:01 -0600

    Add costume fonts to the project

---
+++ app/src/main/java/icu/gralpaprika/barbarian/counter/presentation/shapes/OvalCornerShape.kt
@@ -0,0 +1,22 @@
+package icu.gralpaprika.barbarian.counter.presentation.shapes
+
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.toRect
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+
+class OvalCornerShape : Shape {
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        val path = Path().apply {
+            addOval(size.toRect())
+        }
+        return Outline.Generic(path)
+    }
+}
